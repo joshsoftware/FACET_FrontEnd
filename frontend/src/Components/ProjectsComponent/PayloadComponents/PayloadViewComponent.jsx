7da5f8cee96f1aecfcdc80d0c795d5be7d9ad5d5
Tanmay Thole <tanmaythole@gmail.com>
2022-07-24 17:47:49 +0530

    Payload View component Added

--- frontend/src/Components/ProjectsComponent/PayloadComponents/PayloadViewComponent.jsx
+++ frontend/src/Components/ProjectsComponent/PayloadComponents/PayloadViewComponent.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap';
+import { ViewComponent } from '../../CustomComponents';
 
-const PayloadViewComponent = () => {
+const PayloadViewComponent = ({ data }) => {
     return (
-        <div>PayloadViewComponent</div>
+        <ViewComponent title={data.name}>
+            <Row>
+                <Col md={6} className='pb-4'>
+                    <small><b>Name</b></small>
+                    <div>{data.name}</div>
+                </Col>
+                <Col md={12} className='pb-4'>
+                    <small><b>Payload</b></small>
+                    <pre>{JSON.stringify(data.payload, null, 4)}</pre>
+                </Col>
+                <Col md={12} className='pb-4'>
+                    <small><b>Expected Outcome</b></small>
+                    <pre>{JSON.stringify(data.expected_outcome, null, 2)}</pre>
+                </Col>
+                <Col md={6} className='pb-4'>
+                    <small><b>Created At</b></small>
+                    <div>{new Date(data.created_at).toLocaleString()}</div>
+                </Col>
+                <Col md={6} className='pb-4'>
+                    <small><b>Created By</b></small>
+                    <div>{data.created_by}</div>
+                </Col>
+                <Col md={6} className='pb-4'>
+                    <small><b>Modified At</b></small>
+                    <div>{new Date(data.modified_at).toLocaleString()}</div>
+                </Col>
+                <Col md={6} className='pb-4'>
+                    <small><b>Modified By</b></small>
+                    <div>{data.modified_by}</div>
+                </Col>
+            </Row>
+        </ViewComponent>
     )
 }
 
