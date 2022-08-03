71eb6d78a9252b87ea80c4160db4d4436604cf0e
poojan-2601 <poojannaik88@gmail.com>
2022-08-03 18:07:39 +0530

    Added coming soon, added is_fit_to_run function

--- frontend/src/containers/ProjectContainer/ProjectOverviewContainer/index.jsx
+++ frontend/src/containers/ProjectContainer/ProjectOverviewContainer/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import comingSoon from '../../../assets/images/comingSoon.png'
 
 const ProjectOverviewContainer = () => {
     return (
         <>
-            <div>
-                Hello
+            <div className='d-flex justify-content-center align-items-center w-100'>
+                <img src={comingSoon} alt="Coming Soon"/>
             </div>
         </>
     )
