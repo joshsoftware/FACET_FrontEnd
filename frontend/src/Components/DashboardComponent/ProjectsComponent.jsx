a0fabb3748c0e8820d684d5ac580e8f4bb69360b
Tanmay Thole <tanmaythole@gmail.com>
2022-08-25 12:02:01 +0530

    no projects found image added

--- frontend/src/Components/DashboardComponent/ProjectsComponent.jsx
+++ frontend/src/Components/DashboardComponent/ProjectsComponent.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import ProjectBox from './ProjectBox';
 import ProjectBoxSkeleton from './ProjectBoxSkeleton';
 import './style.css'
+import NotFound from '../../assets/images/notFound.svg';
 
 
 const mapState = ({ projects }) => ({
@@ -14,7 +15,7 @@ const ProjectsComponent = () => {
     const { projects, isLoading } = useSelector(mapState);
 
     return (
-        <div className='project-container py-4'>
+        <div className={`${projects.length===0?'w-100':'project-container'} py-4`}>
             {isLoading?(
                 <>
                     <ProjectBoxSkeleton />
@@ -22,10 +23,19 @@ const ProjectsComponent = () => {
                     <ProjectBoxSkeleton />
                     <ProjectBoxSkeleton />
                 </>
+            ):(projects.length===0?(
+                <div className='d-flex justify-content-center align-items-center'>
+                    <div className='text-center'>
+                        <img src={NotFound} className="not-found-icon" />
+                        <h4>No Projects Found!</h4>
+                        <p>You are not assigned to any project.</p>
+                    </div>
+                </div>
             ):(
                 projects.map((e, index) => {
                     return <ProjectBox key={index} data={e} />
                 })
+            )
             )}
         </div>
     )
