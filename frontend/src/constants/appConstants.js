6fd083d6759cf31a3ef9c948673b5e620165b736
Tanmay Thole <tanmaythole@gmail.com>
2022-09-19 15:34:28 +0530

    changes: expected outcome value field modified

--- frontend/src/constants/appConstants.js
+++ frontend/src/constants/appConstants.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 export const SERVER_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const GET = 'GET';
@@ -5,3 +6,8 @@ export const POST = 'POST';
 export const PUT = 'PUT';
 export const PATCH = 'PATCH';
 export const DELETE = 'DELETE';
+
+export const BOOLEAN_SELECT_OPTION = [
+    {value: true, label: 'True'},
+    {value: false, label: 'False'},
+]
