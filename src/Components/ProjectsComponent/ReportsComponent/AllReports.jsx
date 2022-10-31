46f22644f46af5fdee236d86aa8d8217ee338aa3
Tanmay Thole <tanmaythole@gmail.com>
2022-10-31 18:37:10 +0530

    ADD: custom infinite scroll added using observerAPI

--- src/Components/ProjectsComponent/ReportsComponent/AllReports.jsx
+++ src/Components/ProjectsComponent/ReportsComponent/AllReports.jsx
@@ -1,103 +1,96 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 import { EyeFill } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-import { ViewComponent } from 'Components/CustomComponents';
 import BadgeComponent from 'Components/BadgeComponent';
-import InfiniteScrollComponent from 'Components/CustomComponents/InfiniteScrollComponent';
+import InfiniteScroll from 'Components/InfiniteScroll';
 import NoResultsFound from 'Components/NoResultsFound';
 import TableComponent from 'Components/CustomComponents/TableComponent/index';
+import { ViewComponent } from 'Components/CustomComponents';
 
-const AllReports = (props) => {
-    const { data, isLoading, projectName, onNavigate, fetchMore, totalResults } = props;
-
-    return (
-        <ViewComponent
-            disabledBtns
-            title="Reports"
-        >
-            {!isLoading && data.length === 0 ? (
-                <NoResultsFound
-                    btnLabel="Run New Suite"
-                    btnOnclick={() => onNavigate(`/project/${projectName}/testcases`)}
-                />
-            ) : (
-                <InfiniteScrollComponent
-                    dataLength={data.length} 
-                    next={fetchMore}
-                    hasMore={data.length !== totalResults}
+const AllReports = ({ data, isLoading, projectName, onNavigate, fetchMore, totalResults }) => (
+    <ViewComponent disabledBtns title="Reports">
+        {!isLoading && data.length === 0 ? (
+            <NoResultsFound
+                btnLabel="Run New Suite"
+                btnOnclick={() => onNavigate(`/project/${projectName}/testcases`)}
+            />
+        ) : (
+            <InfiniteScroll
+                dataLength={data.length}
+                next={fetchMore}
+                hasMore={data.length !== totalResults}
+            >
+                <TableComponent
+                    striped
+                    bordered
+                    headings={[
+                        '#',
+                        'Testcase',
+                        'Executed By',
+                        'Total Teststeps',
+                        'Passed',
+                        'Failed',
+                        'Executed On',
+                        '',
+                    ]}
                 >
-                    <TableComponent
-                        striped
-                        bordered
-                        headings={[
-                            "#", 
-                            "Testcase", 
-                            "Executed By",
-                            "Total Teststeps", 
-                            "Passed", 
-                            "Failed", 
-                            "Executed On", 
-                            ""
-                        ]}
-                    >
-                        {data.map((item, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>
-                                        {item.id}
-                                    </td>
-                                    <td>
-                                        <Link to={`${item.id}`} className="text-dark text-decoration-none">
-                                            {item.testcase.name}
-                                        </Link>
-                                    </td>
-                                    <td>{item.executed_by.name}</td>
-                                    <td>{item.no_of_passed_teststeps + item.no_of_failed_teststeps}</td>
-                                    <td className='text-success'>
-                                        <BadgeComponent 
-                                            bg="success"
-                                            label={item.no_of_passed_teststeps}
-                                        />
-                                    </td>
-                                    <td className='text-danger'>
-                                        <BadgeComponent 
-                                            bg="danger"
-                                            label={item.no_of_failed_teststeps}
-                                        />
-                                    </td>
-                                    <td>{new Date(item.executed_on).toLocaleString()}</td>
-                                    <td>
-                                        <Link to={`${item.id}`}>
-                                            <Button
-                                                size='sm'
-                                                variant='secondary'
-                                                className='d-flex justify-content-center align -items-center'
-                                            >
-                                                <EyeFill />
-                                            </Button>
-                                        </Link>
-                                    </td>
-                                </tr>
-                            )
-                        })}
-                    </TableComponent>
-                </InfiniteScrollComponent>
-            )}
-        </ViewComponent>
-    );
-};
+                    {data.map((item, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{item.id}</td>
+                                <td>
+                                    <Link
+                                        to={`${item.id}`}
+                                        className="text-dark text-decoration-none"
+                                    >
+                                        {item.testcase.name}
+                                    </Link>
+                                </td>
+                                <td>{item.executed_by.name}</td>
+                                <td>{item.no_of_passed_teststeps + item.no_of_failed_teststeps}</td>
+                                <td className="text-success">
+                                    <BadgeComponent
+                                        bg="success"
+                                        label={item.no_of_passed_teststeps}
+                                    />
+                                </td>
+                                <td className="text-danger">
+                                    <BadgeComponent
+                                        bg="danger"
+                                        label={item.no_of_failed_teststeps}
+                                    />
+                                </td>
+                                <td>{new Date(item.executed_on).toLocaleString()}</td>
+                                <td>
+                                    <Link to={`${item.id}`}>
+                                        <Button
+                                            size="sm"
+                                            variant="secondary"
+                                            className="d-flex justify-content-center align -items-center"
+                                        >
+                                            <EyeFill />
+                                        </Button>
+                                    </Link>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </TableComponent>
+            </InfiniteScroll>
+        )}
+    </ViewComponent>
+);
 
-AllReports.propTypes = { 
-    data: PropTypes.array, 
-    isLoading: PropTypes.bool, 
-    projectName: PropTypes.string,
-    onNavigate: PropTypes.func,
-    fetchMore: PropTypes.func,
-    totalResults: PropTypes.number,
+AllReports.propTypes = {
+    data: PropTypes.array.isRequired,
+    isLoading: PropTypes.bool,
+    projectName: PropTypes.string.isRequired,
+    onNavigate: PropTypes.func.isRequired,
+    fetchMore: PropTypes.func.isRequired,
+    totalResults: PropTypes.number.isRequired,
 };
 
 export default AllReports;
-
--- src/store/Reports/reducers.js
+++ src/store/Reports/reducers.js
@@ -1,28 +1,28 @@
-import reportsConstants from "./constants";
+import reportsConstants from './constants';
 
 const INITIAL_STATE = {
     isReportsLoading: true,
     reports: [],
     totalResults: 0,
-    page: 1,
+    page: 0,
     pageSize: 20,
     errors: [],
     isOneReportLoading: true,
     singleReport: {},
     isOneTeststepReportLoading: true,
     singleTeststepReport: {},
-    isAddCommentLoading: false
+    isAddCommentLoading: false,
 };
 
-const reportsReducer = (state=INITIAL_STATE, action) => {
+const reportsReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case reportsConstants.GET_REPORTS_REQUEST:
             return {
                 ...state,
                 isReportsLoading: true,
                 page: action.payload.page,
-                reports: action.payload.page === 1 ? [] : state.reports,
-            }
+                reports: action.payload.page === 0 ? [] : state.reports,
+            };
 
         case reportsConstants.GET_REPORTS_SUCCESS:
             return {
@@ -31,77 +31,45 @@ const reportsReducer = (state=INITIAL_STATE, action) => {
                 reports: state.reports.concat(action.payload.results),
                 totalResults: action.payload.total_results,
                 errors: [],
-            }
+            };
 
         case reportsConstants.GET_REPORTS_FAILURE:
-            return {
-                ...state,
-                isReportsLoading: false,
-                errors: action.payload
-            }
+            return { ...state, isReportsLoading: false, errors: action.payload };
 
         case reportsConstants.GET_SINGLE_REPORT_REQUEST:
-            return {
-                ...state,
-                isOneReportLoading: true
-            }
+            return { ...state, isOneReportLoading: true };
 
         case reportsConstants.GET_SINGLE_REPORT_SUCCESS:
-            return {
-                ...state,
-                isOneReportLoading: false,
-                singleReport: action.payload
-            }
+            return { ...state, isOneReportLoading: false, singleReport: action.payload };
 
         case reportsConstants.GET_SINGLE_REPORT_FAILURE:
-            return {
-                ...state,
-                isOneReportLoading: false,
-                errors: action.payload
-            }
+            return { ...state, isOneReportLoading: false, errors: action.payload };
 
         case reportsConstants.GET_TESTSTEP_OF_SINGLE_REPORT_REQUEST:
-            return {
-                ...state,
-                isOneTeststepReportLoading: true
-            }
+            return { ...state, isOneTeststepReportLoading: true };
 
         case reportsConstants.GET_TESTSTEP_OF_SINGLE_REPORT_SUCCESS:
             return {
                 ...state,
                 isOneTeststepReportLoading: false,
-                singleTeststepReport: action.payload
-            }
+                singleTeststepReport: action.payload,
+            };
 
         case reportsConstants.GET_TESTSTEP_OF_SINGLE_REPORT_FAILURE:
-            return {
-                ...state,
-                isOneTeststepReportLoading: false,
-                errors: action.payload
-            }
+            return { ...state, isOneTeststepReportLoading: false, errors: action.payload };
 
         case reportsConstants.ADD_COMMENT_REQUEST:
-            return {
-                ...state,
-                isAddCommentLoading: true
-            }
+            return { ...state, isAddCommentLoading: true };
 
         case reportsConstants.ADD_COMMENT_SUCCESS:
-            return {
-                ...state,
-                isAddCommentLoading: false
-            }
+            return { ...state, isAddCommentLoading: false };
 
         case reportsConstants.ADD_COMMENT_FAILURE:
-            return {
-                ...state,
-                isAddCommentLoading: false,
-                errors: action.payload
-            }
+            return { ...state, isAddCommentLoading: false, errors: action.payload };
 
         default:
             return state;
     }
-}
+};
 
 export default reportsReducer;
