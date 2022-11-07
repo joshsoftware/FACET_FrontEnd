473a143e305796ce900c46284a090ed55bcc5f74
Tanmay Thole <tanmaythole@gmail.com>
2022-11-07 08:28:06 +0530

    code optimize of FormSelect component when isMulti becomes true

--- src/Components/forms/Inputs/FormSelect.jsx
+++ src/Components/forms/Inputs/FormSelect.jsx
@@ -1,88 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { Form } from 'react-bootstrap';
-import Select from 'react-select';
+import React, { useEffect, useState } from "react";
+import { Form } from "react-bootstrap";
+import PropTypes from "prop-types";
+import Select from "react-select";
 
 const FormSelect = (props) => {
-    const {
-        label,
-        name,
-        value,
-        handlechange,
-        text,
-        className,
-        style,
-        isRequired,
-        options,
-        isMulti,
-        placeholder
-    } = props;
-    const [defaultValue, setDefaultValue] = useState();
-    
-    useEffect(() => {
-        if(isMulti) {
-            let vals = value.map(val => val);
-            let res = options.filter(function(item) {
-                return vals.indexOf(item.value) != -1;
-            })
-            setDefaultValue(res);
-        } else {
-            setDefaultValue(options.find(val => val.value===value));
-        }
-    }, [value, options])
+  const {
+    label,
+    name,
+    value,
+    handlechange,
+    text,
+    className,
+    style,
+    isRequired,
+    options,
+    isMulti,
+    placeholder,
+  } = props;
+  const [defaultValue, setDefaultValue] = useState();
 
-    const onChange = (e) => {
-        if(isMulti) {
-            handlechange(name, e)
-        } else {
-            handlechange(name, e.value)
-        }
+  useEffect(() => {
+    if (isMulti) {
+      setDefaultValue(value);
+    } else {
+      setDefaultValue(options.find((val) => val.value === value));
     }
+  }, [value, options]);
 
-    return (
-        <>
-            <Form.Group className={`${className} mb-3`} style={style}>
-                {label&&(
-                    <Form.Label htmlFor={name} className='w-100' >
-                        {label}
-                        {isRequired&&<span className='text-danger'>*</span>}
-                    </Form.Label>
-                )}
-                <Select 
-                    options={options}
-                    onChange={onChange}
-                    value={defaultValue}
-                    isMulti={isMulti}
-                    placeholder={placeholder}
-                />
-                {text && (
-                    <Form.Text className='text-muted'>
-                        {text}
-                    </Form.Text>
-                )}
-            </Form.Group>
-        </>
-    )
-}
+  const onChange = (e) => {
+    if (isMulti) {
+      handlechange(name, e);
+    } else {
+      handlechange(name, e.value);
+    }
+  };
 
-export default FormSelect;
+  return (
+    <>
+      <Form.Group className={`${className} mb-3`} style={style}>
+        {label && (
+          <Form.Label htmlFor={name} className="w-100">
+            {label}
+            {isRequired && <span className="text-danger">*</span>}
+          </Form.Label>
+        )}
+        <Select
+          options={options}
+          onChange={onChange}
+          value={defaultValue}
+          isMulti={isMulti}
+          placeholder={placeholder}
+        />
+        {text && <Form.Text className="text-muted">{text}</Form.Text>}
+      </Form.Group>
+    </>
+  );
+};
 
 FormSelect.propTypes = {
-    label: PropTypes.string,
-    name: PropTypes.string,
-    id: PropTypes.string,
-    value: PropTypes.any,
-    handlechange: PropTypes.func,
-    element: PropTypes.element,
-    text: PropTypes.string,
-    className: PropTypes.string,
-    style: PropTypes.object,
-    isRequired: PropTypes.bool,
-    options: PropTypes.array,
-    isMulti: PropTypes.bool,
-    placeholder: PropTypes.string
-}
+  label: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  value: PropTypes.any,
+  handlechange: PropTypes.func,
+  element: PropTypes.element,
+  text: PropTypes.string,
+  className: PropTypes.string,
+  style: PropTypes.object,
+  isRequired: PropTypes.bool,
+  options: PropTypes.array,
+  isMulti: PropTypes.bool,
+  placeholder: PropTypes.string,
+};
 
-FormSelect.defaultProps = {
-    isMulti: false
-}
+export default FormSelect;
--- src/containers/ProjectContainer/MemberContainer/index.jsx
+++ src/containers/ProjectContainer/MemberContainer/index.jsx
@@ -1,136 +1,147 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import { NavDropdown } from 'react-bootstrap';
-import { PersonCircle } from 'react-bootstrap-icons';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { NavDropdown } from "react-bootstrap";
+import { PersonCircle } from "react-bootstrap-icons";
+import { useDispatch, useSelector } from "react-redux";
 
-import { ViewComponent } from 'Components/CustomComponents';
-import AddMembersInProject from 'Components/DashboardComponent/ProjectAdmin/AddMembersInProject';
-import { AddButton } from 'Components/forms/Buttons';
-import { addMembersInProjectRequest, getProjectMembersRequest, removeMembersInProjectRequest } from 'store/Projects/actions';
-import { getAllUsersRequest } from 'store/User/actions';
-import TableComponent from 'Components/CustomComponents/TableComponent/index';
+import { ViewComponent } from "Components/CustomComponents";
+import AddMembersInProject from "Components/DashboardComponent/ProjectAdmin/AddMembersInProject";
+import { AddButton } from "Components/forms/Buttons";
+import {
+  addMembersInProjectRequest,
+  getProjectMembersRequest,
+  removeMembersInProjectRequest,
+} from "store/Projects/actions";
+import { getAllUsersRequest } from "store/User/actions";
+import TableComponent from "Components/CustomComponents/TableComponent/index";
 
 const mapState = ({ projectMembers, user, getUsers }) => ({
-    members: projectMembers.members,
-    project: projectMembers.project,
-    isLoading: projectMembers.isLoading,
-    project_admin: projectMembers.project_admin,
-    user: user.currentUser,
-    allUsers: getUsers.users
-})
+  members: projectMembers.members,
+  project: projectMembers.project,
+  isLoading: projectMembers.isLoading,
+  project_admin: projectMembers.project_admin,
+  user: user.currentUser,
+  allUsers: getUsers.users,
+});
 
 const MemberContainer = () => {
-    let dispatch = useDispatch();
+  let dispatch = useDispatch();
 
-    const { projectName } = useParams();
-    const { members, isLoading, project_admin, user, allUsers } = useSelector(mapState);
-    
-    const [show, setShow] = useState(false);
-    const [addMemberFormData, setAddMemberFormData] = useState({project: projectName, members: []});
-    const [usersOptions, setUsersOptions] = useState([]);
+  const { projectName } = useParams();
+  const { members, isLoading, project_admin, user, allUsers } =
+    useSelector(mapState);
 
-    const toggleModal = () => {
-        setShow(!show);
-    }
-    useEffect(() => {
-        dispatch(getProjectMembersRequest({project: projectName}))
-    }, [projectName])
-    
-    useEffect(() => {
-        if (projectName) {
-            dispatch(getAllUsersRequest({exclude: "projectMembers", project: projectName}))
-        }
-    }, [projectName])
-    
-    useEffect(() => {
-        let options_data = [];
-        if(allUsers){
-            allUsers.forEach(ele => {
-                options_data.push({value: ele.id, label: ele.name})
-            })
-        }
-        setUsersOptions(options_data);
-    }, [allUsers])
-    
-    const removeMember = (id) => {
-        dispatch(removeMembersInProjectRequest({project: projectName, members: [id]}))
-    }
+  const [show, setShow] = useState(false);
+  const [addMemberFormData, setAddMemberFormData] = useState({
+    project: projectName,
+    members: [],
+  });
+  const [usersOptions, setUsersOptions] = useState([]);
 
-    const onchangeMembers = (_name, val) => {
-        console.log(val)
-        setAddMemberFormData(p => ({
-            ...p,
-            members: val.map(e => e.value),
-        }))
+  const toggleModal = () => {
+    setShow(!show);
+  };
+
+  useEffect(() => {
+    dispatch(getProjectMembersRequest({ project: projectName }));
+  }, [projectName]);
+
+  useEffect(() => {
+    if (projectName) {
+      dispatch(
+        getAllUsersRequest({ exclude: "projectMembers", project: projectName })
+      );
     }
-    const onSubmitMemberForm = () => {
-        dispatch(addMembersInProjectRequest(addMemberFormData));
-        toggleModal();
+  }, [projectName]);
+
+  useEffect(() => {
+    let options_data = [];
+    if (allUsers) {
+      allUsers.forEach((ele) => {
+        options_data.push({ value: ele.id, label: ele.name });
+      });
     }
-    
-    return (
-        <>
-            {show&&(
-                <AddMembersInProject 
-                    show={show}
-                    handleClose={toggleModal}
-                    usersOptions={usersOptions} 
-                    onchange={onchangeMembers}
-                    value={addMemberFormData.members}
-                    handleSubmit={onSubmitMemberForm}
-                />
-            )}
-            <div className='py-5 w-100'>
-                <div className='d-flex justify-content-between align-items-center px-5'>
-                    <h3>Team Members</h3>
-                    {project_admin===user.id&&(
-                        <AddButton 
-                            label="Add Member"
-                            handleClick={() => setShow(true)}
-                        />
-                    )}
-                </div>
-                <ViewComponent
-                    disabledHeader
-                >
-                    <TableComponent striped headings={["#", "Member", "Role", "Actions"]} >
-                        {!isLoading&&(
-                            members.map((item, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{index+1}</td>
-                                        <td>
-                                            <PersonCircle 
-                                                size={24}
-                                                className="mx-2"
-                                            />
-                                            {item.name}
-                                        </td>
-                                        <td>{item.is_project_admin?"Admin":"Member"}</td>
-                                        <td>
-                                            <NavDropdown 
-                                                title="More" 
-                                                disabled={project_admin!==user.id || project_admin===item.id}
-                                                style={{lineHeight: "10px"}}
-                                            >
-                                                <NavDropdown.Item 
-                                                    onClick={() => removeMember(item.id)}
-                                                    style={{lineHeight: 'initial'}}
-                                                >
-                                                    Remove from Project
-                                                </NavDropdown.Item>
-                                            </NavDropdown>
-                                        </td>
-                                    </tr>
-                                )
-                            })
-                        )}
-                    </TableComponent>
-                </ViewComponent>
-            </div>
-        </>
-    )
-}
+    setUsersOptions(options_data);
+  }, [allUsers]);
+
+  const removeMember = (id) => {
+    dispatch(
+      removeMembersInProjectRequest({ project: projectName, members: [id] })
+    );
+  };
+
+  const onchangeMembers = (_name, val) => {
+    setAddMemberFormData((p) => ({
+      ...p,
+      members: val,
+    }));
+  };
+
+  const onSubmitMemberForm = () => {
+    dispatch(
+      addMembersInProjectRequest({
+        ...addMemberFormData,
+        members: addMemberFormData.members.map((e) => e.value),
+      })
+    );
+    toggleModal();
+  };
+
+  return (
+    <>
+      {show && (
+        <AddMembersInProject
+          show={show}
+          handleClose={toggleModal}
+          usersOptions={usersOptions}
+          onchange={onchangeMembers}
+          value={addMemberFormData.members}
+          handleSubmit={onSubmitMemberForm}
+        />
+      )}
+      <div className="py-5 w-100">
+        <div className="d-flex justify-content-between align-items-center px-5">
+          <h3>Team Members</h3>
+          {project_admin === user.id && (
+            <AddButton label="Add Member" handleClick={() => setShow(true)} />
+          )}
+        </div>
+        <ViewComponent disabledHeader>
+          <TableComponent striped headings={["#", "Member", "Role", "Actions"]}>
+            {!isLoading &&
+              members.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <PersonCircle size={24} className="mx-2" />
+                      {item.name}
+                    </td>
+                    <td>{item.is_project_admin ? "Admin" : "Member"}</td>
+                    <td>
+                      <NavDropdown
+                        title="More"
+                        disabled={
+                          project_admin !== user.id || project_admin === item.id
+                        }
+                        style={{ lineHeight: "10px" }}
+                      >
+                        <NavDropdown.Item
+                          onClick={() => removeMember(item.id)}
+                          style={{ lineHeight: "initial" }}
+                        >
+                          Remove from Project
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                    </td>
+                  </tr>
+                );
+              })}
+          </TableComponent>
+        </ViewComponent>
+      </div>
+    </>
+  );
+};
 
 export default MemberContainer;
