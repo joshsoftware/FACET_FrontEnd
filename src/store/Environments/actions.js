588cf01721838a454abc7f07960c4395e05ae477
Tanmay Thole <tanmaythole@gmail.com>
2022-11-29 15:25:46 +0530

    Environments error messages updated in sagas, environments logic in sagas improved

--- src/store/Environments/actions.js
+++ src/store/Environments/actions.js
@@ -1,61 +1,59 @@
 import environmentConstants from "./constants";
 
 export const getEnvironmentsRequest = (data) => ({
-    type: environmentConstants.GET_ENVIRONMENTS_REQUEST,
-    payload: data
-})
+  type: environmentConstants.GET_ENVIRONMENTS_REQUEST,
+  payload: data,
+});
 
 export const getEnvironmentsSuccess = (data) => ({
-    type: environmentConstants.GET_ENVIRONMENTS_SUCCESS,
-    payload: data
-})
+  type: environmentConstants.GET_ENVIRONMENTS_SUCCESS,
+  payload: data,
+});
 
 export const getEnvironmentsFailure = (data) => ({
-    type: environmentConstants.GET_ENVIRONMENTS_FAILURE,
-    payload: data
-})
+  type: environmentConstants.GET_ENVIRONMENTS_FAILURE,
+  payload: data,
+});
 
 export const addEnvironmentsRequest = (data) => ({
-    type: environmentConstants.ADD_ENVIRONMENTS_REQUEST,
-    payload: data
-})
+  type: environmentConstants.ADD_ENVIRONMENTS_REQUEST,
+  payload: data,
+});
 
-export const addEnvironmentsSuccess = (data) => ({
-    type: environmentConstants.ADD_ENVIRONMENTS_SUCCESS,
-    payload: data
-})
+export const addEnvironmentsSuccess = () => ({
+  type: environmentConstants.ADD_ENVIRONMENTS_SUCCESS,
+});
 
 export const addEnvironmentsFailure = (data) => ({
-    type: environmentConstants.ADD_ENVIRONMENTS_FAILURE,
-    payload: data
-})
+  type: environmentConstants.ADD_ENVIRONMENTS_FAILURE,
+  payload: data,
+});
 
 export const editEnvironmentsRequest = (data) => ({
-    type: environmentConstants.EDIT_ENVIRONMENTS_REQUEST,
-    payload: data
-})
+  type: environmentConstants.EDIT_ENVIRONMENTS_REQUEST,
+  payload: data,
+});
 
-export const editEnvironmentsSuccess = (data) => ({
-    type: environmentConstants.EDIT_ENVIRONMENTS_SUCCESS,
-    payload: data
-})
+export const editEnvironmentsSuccess = () => ({
+  type: environmentConstants.EDIT_ENVIRONMENTS_SUCCESS,
+});
 
 export const editEnvironmentsFailure = (data) => ({
-    type: environmentConstants.EDIT_ENVIRONMENTS_FAILURE,
-    payload: data
-})
+  type: environmentConstants.EDIT_ENVIRONMENTS_FAILURE,
+  payload: data,
+});
 
 export const deleteEnvironmentsRequest = (data) => ({
-    type: environmentConstants.DELETE_ENVIRONMENTS_REQUEST,
-    payload: data
-})
+  type: environmentConstants.DELETE_ENVIRONMENTS_REQUEST,
+  payload: data,
+});
 
 export const deleteEnvironmentsSuccess = (data) => ({
-    type: environmentConstants.DELETE_ENVIRONMENTS_SUCCESS,
-    payload: data
-})
+  type: environmentConstants.DELETE_ENVIRONMENTS_SUCCESS,
+  payload: data,
+});
 
 export const deleteEnvironmentsFailure = (data) => ({
-    type: environmentConstants.DELETE_ENVIRONMENTS_FAILURE,
-    payload: data
-})
+  type: environmentConstants.DELETE_ENVIRONMENTS_FAILURE,
+  payload: data,
+});
--- src/store/Environments/reducers.js
+++ src/store/Environments/reducers.js
@@ -1,29 +1,43 @@
 import environmentConstants from "./constants";
 
-const INITIAL_STATE = {
-    isLoading: true,
-    environments: [],
-    errors: []
-}
+const initialState = {
+  isLoading: false,
+  environments: [],
+  errors: [],
+};
 
-const environmentReducer = (state=INITIAL_STATE, action) => {
-    switch (action.type) {
-        case environmentConstants.GET_ENVIRONMENTS_SUCCESS:
-            return {
-                ...state,
-                isLoading: false,
-                environments: action.payload
-            }
-        
-        case environmentConstants.GET_ENVIRONMENTS_FAILURE:
-            return {
-                ...state,
-                errors: action.payload
-            }
-    
-        default:
-            return state;
-    }
-}
+const environmentReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case environmentConstants.GET_ENVIRONMENTS_REQUEST:
+      return { ...state, isLoading: true };
+
+    case environmentConstants.GET_ENVIRONMENTS_SUCCESS:
+      return { ...state, isLoading: false, environments: action.payload };
+
+    case environmentConstants.GET_ENVIRONMENTS_FAILURE:
+      return { ...state, isLoading: false, errors: action.payload };
+
+    case environmentConstants.ADD_ENVIRONMENTS_REQUEST:
+      return { ...state, isLoading: true };
+
+    case environmentConstants.ADD_ENVIRONMENTS_SUCCESS:
+      return { ...state, isLoading: false };
+
+    case environmentConstants.ADD_ENVIRONMENTS_FAILURE:
+      return { ...state, isLoading: false, errors: action.payload };
+
+    case environmentConstants.EDIT_ENVIRONMENTS_REQUEST:
+      return { ...state, isLoading: true };
+
+    case environmentConstants.EDIT_ENVIRONMENTS_SUCCESS:
+      return { ...state, isLoading: false };
+
+    case environmentConstants.EDIT_ENVIRONMENTS_FAILURE:
+      return { ...state, isLoading: false, errors: action.payload };
+
+    default:
+      return state;
+  }
+};
 
 export default environmentReducer;
