2feca8713db543322a3e8cd79605b3267a9ab765
Tanmay Thole <tanmaythole@gmail.com>
2022-11-30 15:37:26 +0530

    Header's saga error handling and toastMessages updated

--- src/store/Headers/reducers.js
+++ src/store/Headers/reducers.js
@@ -1,31 +1,21 @@
 import headerConstants from "./constants";
 
-const INITIAL_STATE = {
-    isLoading: true,
-    headers: [],
-    errors: []
-}
+const initialState = { isLoading: false, headers: [], errors: "" };
 
-const headerReducer = (state=INITIAL_STATE, action) => {
-    switch (action.type) {
-        case headerConstants.GET_HEADERS_SUCCESS:
-            return {
-                ...state,
-                isLoading: false,
-                headers: action.payload,
-                errors: []
-            }
-    
-        case headerConstants.GET_HEADERS_FAILURE:
-            return {
-                ...state,
-                isLoading: false,
-                errors: action.payload
-            }
-            
-        default:
-            return state;
-    }
-}
+const headerReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case headerConstants.GET_HEADERS_REQUEST:
+      return { ...state, isLoading: true };
+
+    case headerConstants.GET_HEADERS_SUCCESS:
+      return { ...state, isLoading: false, headers: action.payload };
+
+    case headerConstants.GET_HEADERS_FAILURE:
+      return { ...state, isLoading: false, errors: action.payload };
+
+    default:
+      return state;
+  }
+};
 
 export default headerReducer;
--- src/store/Headers/sagas.js
+++ src/store/Headers/sagas.js
@@ -1,41 +1,51 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
-import { getHeadersSuccess } from './actions';
-import { addHeaderApi, editHeaderApi, getHeadersApi } from './apis';
-import headerConstants from './constants';
-import { toast } from 'react-toastify';
+import { call, put, takeLatest } from "redux-saga/effects";
 
+import { getHeadersFailure, getHeadersSuccess } from "./actions";
+import { addHeaderApi, editHeaderApi, getHeadersApi } from "./apis";
+import headerConstants from "./constants";
+
+import { HEADERS } from "constants/userMessagesConstants";
+import { toastMessage } from "utils/toastMessage";
+import { apisErrorMessage } from "utils/apisErrorMessage";
 
 export function* getHeaders({ payload }) {
-    try {
-        const response = yield call(getHeadersApi, payload);
-        yield put(getHeadersSuccess(response.headers));
-    } catch (error) {
-        toast.error(error.data.error)
-    }
+  try {
+    const response = yield call(getHeadersApi, payload);
+    yield put(getHeadersSuccess(response.headers));
+  } catch (error) {
+    const errorMessage = apisErrorMessage(error);
+    toastMessage(errorMessage, "error");
+    yield put(getHeadersFailure(errorMessage));
+  }
 }
 
 export function* addHeader({ payload }) {
-    try {
-        yield call(addHeaderApi, payload);
-        toast.success("Header Added Successfully!")
-        yield call(getHeaders, {payload: { project: payload.project }});
-    } catch (error) {
-        toast.error(error.data.error)
-    }
+  try {
+    yield call(addHeaderApi, payload);
+    toastMessage(HEADERS.ADD_NEW_SUCCESS, "success");
+    yield call(getHeaders, { payload: { project: payload.project } });
+  } catch (error) {
+    const errorMessage = apisErrorMessage(error);
+    toastMessage(errorMessage, "error");
+    yield put(getHeadersFailure(errorMessage));
+  }
 }
 
 export function* editHeader({ payload }) {
-    try {
-        yield call(editHeaderApi, payload);
-        toast.success("Header Updated Successfully!")
-        yield call(getHeaders, {payload: { project: payload.project }});
-    } catch (error) {
-        toast.error(error.data.error)
-    }
+  try {
+    yield call(editHeaderApi, payload);
+    toastMessage(HEADERS.UPDATE_SUCCESS, "success");
+    yield call(getHeaders, { payload: { project: payload.project } });
+  } catch (error) {
+    const errorMessage = apisErrorMessage(error);
+    toastMessage(errorMessage, "error");
+    yield put(getHeadersFailure(errorMessage));
+  }
 }
 
+// Watcher saga
 export default function* headerSagas() {
-    yield takeLatest(headerConstants.GET_HEADERS_REQUEST, getHeaders);
-    yield takeLatest(headerConstants.ADD_HEADERS_REQUEST, addHeader);
-    yield takeLatest(headerConstants.EDIT_HEADERS_REQUEST, editHeader);
+  yield takeLatest(headerConstants.GET_HEADERS_REQUEST, getHeaders);
+  yield takeLatest(headerConstants.ADD_HEADERS_REQUEST, addHeader);
+  yield takeLatest(headerConstants.EDIT_HEADERS_REQUEST, editHeader);
 }
