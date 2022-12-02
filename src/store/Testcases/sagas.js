5d6a7cd1d287287ede16c1c1256ededf2f3c9f36
Tanmay Thole <tanmaythole@gmail.com>
2022-12-02 11:47:03 +0530

    reducers and sagas added in testcases store

--- src/store/Testcases/sagas.js
+++ src/store/Testcases/sagas.js
@@ -1,41 +1,56 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
-import { getTestcasesSuccess } from './actions';
-import { addTestcaseApi, editTestcaseApi, getTestcasesApi } from './apis';
-import testcaseConstants from './constants';
-import { toast } from 'react-toastify';
+import { call, put, takeLatest } from "redux-saga/effects";
 
+import { addTestcaseApi, editTestcaseApi, getTestcasesApi } from "./apis";
+import {
+  addTestcasesFailure,
+  editTestcasesFailure,
+  getTestcasesFailure,
+  getTestcasesSuccess,
+} from "./actions";
+import testcaseConstants from "./constants";
+
+import { TESTCASES } from "constants/userMessagesConstants";
+import { apisErrorMessage } from "utils/apisErrorMessage";
+import { toastMessage } from "utils/toastMessage";
 
 export function* getTestcases({ payload }) {
-    try {
-        const response = yield call(getTestcasesApi, payload);
-        yield put(getTestcasesSuccess(response.testcases));
-    } catch (error) {
-        toast.error(error.data.error)
-    }
+  try {
+    const response = yield call(getTestcasesApi, payload);
+    yield put(getTestcasesSuccess(response.testcases));
+  } catch (error) {
+    const errorMessage = apisErrorMessage(error);
+    toastMessage(errorMessage, "error");
+    yield put(getTestcasesFailure(errorMessage));
+  }
 }
 
 export function* addTestcase({ payload }) {
-    try {
-        yield call(addTestcaseApi, payload);
-        toast.success("Testcase Added Successfully!")
-        yield call(getTestcases, {payload: { project: payload.project }});
-    } catch (error) {
-        toast.error(error.data.error)
-    }
+  try {
+    yield call(addTestcaseApi, payload);
+    toastMessage(TESTCASES.ADD_NEW_SUCCESS, "success");
+    yield call(getTestcases, { payload: { project: payload.project } });
+  } catch (error) {
+    const errorMessage = apisErrorMessage(error);
+    toastMessage(errorMessage, "error");
+    yield put(addTestcasesFailure(errorMessage));
+  }
 }
 
 export function* editTestcase({ payload }) {
-    try {
-        yield call(editTestcaseApi, payload);
-        toast.success("Testcase Updated Successfully!")
-        yield call(getTestcases, {payload: { project: payload.project }});
-    } catch (error) {
-        toast.error(error.data.error)
-    }
+  try {
+    yield call(editTestcaseApi, payload);
+    toastMessage(TESTCASES.UPDATE_SUCCESS, "success");
+    yield call(getTestcases, { payload: { project: payload.project } });
+  } catch (error) {
+    const errorMessage = apisErrorMessage(error);
+    toastMessage(errorMessage, "error");
+    yield put(editTestcasesFailure(errorMessage));
+  }
 }
 
+// Watcher Saga
 export default function* testcaseSagas() {
-    yield takeLatest(testcaseConstants.GET_TESTCASES_REQUEST, getTestcases);
-    yield takeLatest(testcaseConstants.ADD_TESTCASES_REQUEST, addTestcase);
-    yield takeLatest(testcaseConstants.EDIT_TESTCASES_REQUEST, editTestcase);
+  yield takeLatest(testcaseConstants.GET_TESTCASES_REQUEST, getTestcases);
+  yield takeLatest(testcaseConstants.ADD_TESTCASES_REQUEST, addTestcase);
+  yield takeLatest(testcaseConstants.EDIT_TESTCASES_REQUEST, editTestcase);
 }
