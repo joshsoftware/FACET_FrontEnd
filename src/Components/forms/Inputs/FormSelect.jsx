2ec1d7f205a2203ffb4896fddfcfcf43402e2066
Tanmay Thole <tanmaythole@gmail.com>
2022-12-12 14:56:55 +0530

    form select component code refactored

--- src/Components/forms/Inputs/FormSelect.jsx
+++ src/Components/forms/Inputs/FormSelect.jsx
@@ -1,76 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Form } from "react-bootstrap";
-import PropTypes from "prop-types";
 import Select from "react-select";
+import PropTypes from "prop-types";
 
-const FormSelect = (props) => {
-  const {
-    label,
-    name,
-    value,
-    handlechange,
-    text,
-    className,
-    style,
-    isRequired,
-    options,
-    isMulti,
-    placeholder,
-  } = props;
-  const [defaultValue, setDefaultValue] = useState();
-
-  useEffect(() => {
-    if (isMulti) {
-      setDefaultValue(value);
-    } else {
-      setDefaultValue(options.find((val) => val.value === value));
-    }
-  }, [value, options]);
-
-  const onChange = (e) => {
-    if (isMulti) {
-      handlechange(name, e);
-    } else {
-      handlechange(name, e.value);
-    }
-  };
-
-  return (
-    <>
-      <Form.Group className={`${className} mb-3`} style={style}>
-        {label && (
-          <Form.Label htmlFor={name} className="w-100">
-            {label}
-            {isRequired && <span className="text-danger">*</span>}
-          </Form.Label>
-        )}
-        <Select
-          options={options}
-          onChange={onChange}
-          value={defaultValue}
-          isMulti={isMulti}
-          placeholder={placeholder}
-        />
-        {text && <Form.Text className="text-muted">{text}</Form.Text>}
-      </Form.Group>
-    </>
-  );
-};
+const FormSelect = ({
+  label,
+  value,
+  onChange,
+  text,
+  className,
+  style,
+  isRequired,
+  options,
+  isMulti,
+  placeholder,
+  isLoading,
+  isDisabled,
+}) => (
+  <>
+    <Form.Group className={`${className} mb-3`} style={style}>
+      {label && (
+        <Form.Label className="w-100">
+          {label}
+          {isRequired && <span className="text-danger">*</span>}
+        </Form.Label>
+      )}
+      <Select
+        options={options}
+        onChange={onChange}
+        value={value}
+        isMulti={isMulti}
+        placeholder={placeholder}
+        isLoading={isLoading}
+        isDisabled={isDisabled}
+      />
+      {text && <Form.Text className="text-muted">{text}</Form.Text>}
+    </Form.Group>
+  </>
+);
 
 FormSelect.propTypes = {
   label: PropTypes.string,
-  name: PropTypes.string,
-  id: PropTypes.string,
   value: PropTypes.any,
-  handlechange: PropTypes.func,
-  element: PropTypes.element,
+  onChange: PropTypes.func.isRequired,
   text: PropTypes.string,
   className: PropTypes.string,
   style: PropTypes.object,
   isRequired: PropTypes.bool,
-  options: PropTypes.array,
+  options: PropTypes.arrayOf(PropTypes.object).isRequired,
   isMulti: PropTypes.bool,
   placeholder: PropTypes.string,
+  isLoading: PropTypes.bool,
+  isDisabled: PropTypes.bool,
 };
 
 export default FormSelect;
