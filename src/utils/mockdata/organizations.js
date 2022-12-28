ca3b40dbeb81b6572f04dcd273484632013240f0
Tanmay Thole <tanmaythole@gmail.com>
2022-12-28 13:13:00 +0530

    mock data for organisations added

---
+++ src/utils/mockdata/organizations.js
@@ -0,0 +1,20 @@
+const organizationsData = {
+  organizations: [
+    {
+      id: 1,
+      name: "sample-org-1",
+      image: "",
+      description: "sample org description",
+      is_owner: true,
+    },
+    {
+      id: 2,
+      name: "sample-org-2",
+      image: "",
+      description: "sample org 2 description",
+      is_owner: false,
+    },
+  ],
+};
+
+export default organizationsData;
