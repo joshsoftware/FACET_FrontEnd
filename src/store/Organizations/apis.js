44ec32383c9f9e1bbd5145258c2e8b207feba963
Tanmay Thole <tanmaythole@gmail.com>
2022-12-28 13:14:48 +0530

    redux saga created for getOrganisations list with help of mockData in apis

---
+++ src/store/Organizations/apis.js
@@ -0,0 +1,7 @@
+// TO-DO
+// Currently this apis file functions returns mockdata
+// When Apis are available then this functions will be replaced
+// by api calls
+import organizationsData from "utils/mockdata/organizations";
+
+export const getOrganizationsApi = () => organizationsData;
