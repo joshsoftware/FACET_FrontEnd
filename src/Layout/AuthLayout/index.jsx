e8e5caea3e3110305c7522a207acb0801a9d6131
Tanmay Thole <tanmaythole@gmail.com>
2023-01-09 18:28:27 +0530

    username field added and auth layout styles fixed

--- src/Layout/AuthLayout/index.jsx
+++ src/Layout/AuthLayout/index.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import PropTypes from "prop-types";
 
+import "./style.css";
+
 const AuthLayout = ({ children }) => (
-  <div className="h-100 d-flex justify-content-center align-items-center">
-    <div className="col-md-4">
+  <div className="d-flex justify-content-center align-items-center authLayout">
+    <div className="col-md-4 py-5">
       <h1 className="text-uppercase text-center">Facet</h1>
       <Card className="p-4">{children}</Card>
     </div>
