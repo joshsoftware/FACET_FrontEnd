9e795f7e9986f6ec20873e29c4f79b6eb7de037f
Tanmay Thole <tanmaythole@gmail.com>
2023-01-16 16:06:33 +0530

    navigate to add organization page if organization is not assigned to the user

--- src/PrivateRoutes.jsx
+++ src/PrivateRoutes.jsx
@@ -1,22 +1,38 @@
 import React, { useEffect } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import Header from "Components/Header";
 
 import { getUserProfileRequest } from "store/User/actions";
 
-const mapState = (state) => ({
-  isLoggedIn: state.user.isLoggedIn,
+import { ADD_ORGANIZATION_ROUTE, LOGIN_ROUTE } from "constants/routeConstants";
+
+const mapState = ({ user }) => ({
+  isLoggedIn: user.isLoggedIn,
+  isOrgAssigned: !!user.currentUser?.user_organization,
 });
 
 const PrivateRoutes = () => {
-  const { isLoggedIn } = useSelector(mapState);
-  let dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const { isLoggedIn, isOrgAssigned } = useSelector(mapState);
 
   useEffect(() => {
     isLoggedIn && dispatch(getUserProfileRequest());
   }, []);
+
+  // check whether if user is loggedin and assigned to org or not
+  // if not assigned to any org and current page location is not the ADD_ORGANIZATION_ROUTE
+  // then user will redirect to ADD_ORGANIZATION_ROUTE
+  if (
+    isLoggedIn &&
+    !isOrgAssigned &&
+    location.pathname !== ADD_ORGANIZATION_ROUTE
+  ) {
+    return <Navigate to={ADD_ORGANIZATION_ROUTE} />;
+  }
 
   return isLoggedIn ? (
     <div>
@@ -24,7 +40,7 @@ const PrivateRoutes = () => {
       <Outlet />
     </div>
   ) : (
-    <Navigate to="/login" replace />
+    <Navigate to={LOGIN_ROUTE} replace />
   );
 };
 
