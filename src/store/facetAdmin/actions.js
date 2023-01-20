796ad2c799dab83b04c1dffab6b9186a0065c0e1
Tanmay Thole <tanmaythole@gmail.com>
2023-01-20 13:26:41 +0530

    constants modified in facetAdmin sagas

--- src/store/facetAdmin/actions.js
+++ src/store/facetAdmin/actions.js
@@ -1,41 +1,41 @@
 import facetAdminConstants from "./constants";
 
 export const getOrganizationsRequest = () => ({
-  type: facetAdminConstants.GET_ORGANIZATIONS_REQUEST,
+  type: facetAdminConstants.GET_ORGANIZATIONS_ADMIN_REQUEST,
 });
 
 export const getOrganizationsSuccess = (data) => ({
-  type: facetAdminConstants.GET_ORGANIZATIONS_SUCCESS,
+  type: facetAdminConstants.GET_ORGANIZATIONS_ADMIN_SUCCESS,
   payload: data,
 });
 
 export const getOrganizationsFailure = () => ({
-  type: facetAdminConstants.GET_ORGANIZATIONS_FAILURE,
+  type: facetAdminConstants.GET_ORGANIZATIONS_ADMIN_FAILURE,
 });
 
 export const getOrganizationRequest = (data) => ({
-  type: facetAdminConstants.GET_ORGANIZATION_REQUEST,
+  type: facetAdminConstants.GET_ORGANIZATION_ADMIN_REQUEST,
   payload: data,
 });
 
 export const getOrganizationSuccess = (data) => ({
-  type: facetAdminConstants.GET_ORGANIZATION_SUCCESS,
+  type: facetAdminConstants.GET_ORGANIZATION_ADMIN_SUCCESS,
   payload: data,
 });
 
 export const getOrganizationFailure = () => ({
-  type: facetAdminConstants.GET_ORGANIZATION_FAILURE,
+  type: facetAdminConstants.GET_ORGANIZATION_ADMIN_FAILURE,
 });
 
 export const getUsersRequest = () => ({
-  type: facetAdminConstants.GET_USERS_REQUEST,
+  type: facetAdminConstants.GET_USERS_ADMIN_REQUEST,
 });
 
 export const getUsersSuccess = (data) => ({
-  type: facetAdminConstants.GET_USERS_SUCCESS,
+  type: facetAdminConstants.GET_USERS_ADMIN_SUCCESS,
   payload: data,
 });
 
 export const getUsersFailure = () => ({
-  type: facetAdminConstants.GET_USERS_FAILURE,
+  type: facetAdminConstants.GET_USERS_ADMIN_FAILURE,
 });
