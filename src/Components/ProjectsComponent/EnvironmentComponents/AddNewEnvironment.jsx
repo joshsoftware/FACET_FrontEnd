ef3b003ccd3043130452dd834c8866826ed4297e
Tanmay Thole <tanmaythole@gmail.com>
2023-02-21 17:36:32 +0530

    max length validation added on name field of environment form

--- src/Components/ProjectsComponent/EnvironmentComponents/AddNewEnvironment.jsx
+++ src/Components/ProjectsComponent/EnvironmentComponents/AddNewEnvironment.jsx
@@ -3,9 +3,12 @@ import { Form } from "react-bootstrap";
 import PropTypes from "prop-types";
 
 import { FormInput } from "Components/forms/Inputs";
+import Loader from "Components/Loader";
 import { ViewComponent } from "Components/CustomComponents";
 
 import { convertToSlug } from "utils";
+
+import { NAME_FIELD_MAX_LENGTH } from "constants/appConstants";
 
 const AddNewEnvironment = ({
   cat,
@@ -16,45 +19,53 @@ const AddNewEnvironment = ({
 }) => {
   const { name, url } = data;
 
+  const isEditForm = cat === "edit";
+
   // check whether all form fields are filled or not, if not then disabled the submit button
-  const isSaveButtonDisabled = !name.length || !url.length;
+  const isSaveButtonDisabled = isLoading || !name.length || !url.length;
 
   const nameInputBottomTextMsg =
     !!name.length &&
     `Your environment will be created as ${convertToSlug(name)}`;
 
+  const viewComponentTitle = isEditForm ? name : "Add New Environment";
+
   return (
-    !isLoading &&
-    data && (
-      <Form onSubmit={handleSubmit} className="w-100">
-        <ViewComponent
-          title="Add New"
-          type="save"
-          onSave={handleSubmit}
-          isSaveDisabled={isSaveButtonDisabled}
-        >
-          <FormInput
-            label="Name"
-            placeholder="Name"
-            name="name"
-            value={name}
-            onChange={onchange}
-            isRequired
-            disabled={cat === "edit"}
-            text={nameInputBottomTextMsg}
-          />
-          <FormInput
-            label="URL"
-            placeholder="Enter URL"
-            name="url"
-            value={url}
-            onChange={onchange}
-            type="url"
-            isRequired
-          />
-        </ViewComponent>
-      </Form>
-    )
+    <Form onSubmit={handleSubmit} className="w-100">
+      <ViewComponent
+        title={viewComponentTitle}
+        type="save"
+        onSave={handleSubmit}
+        isSaveDisabled={isSaveButtonDisabled}
+      >
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <FormInput
+              label="Name"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={onchange}
+              disabled={isEditForm}
+              text={nameInputBottomTextMsg}
+              maxLength={NAME_FIELD_MAX_LENGTH}
+              isRequired
+            />
+            <FormInput
+              label="URL"
+              placeholder="Enter URL"
+              name="url"
+              value={url}
+              onChange={onchange}
+              type="url"
+              isRequired
+            />
+          </>
+        )}
+      </ViewComponent>
+    </Form>
   );
 };
 
