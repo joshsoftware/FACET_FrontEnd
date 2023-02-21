ae0526b555e2363af689aec2146c9727894bbbfc
Tanmay Thole <tanmaythole@gmail.com>
2023-02-21 18:37:05 +0530

    max length validation added on name field of testsuite form

--- src/Components/ProjectsComponent/TestsuiteComponent/AddNewTestsuite.jsx
+++ src/Components/ProjectsComponent/TestsuiteComponent/AddNewTestsuite.jsx
@@ -4,19 +4,24 @@ import PropTypes from "prop-types";
 
 import FormInput from "Components/forms/Inputs/FormInput";
 import FormSelect from "Components/forms/Inputs/FormSelect";
+import Loader from "Components/Loader";
 import { ViewComponent } from "Components/CustomComponents";
 
 import convertToSlug from "utils/convertToSlug";
-import { formTitle } from "utils/helper";
+
+import { NAME_FIELD_MAX_LENGTH } from "constants/appConstants";
 
 const AddNewTestsuite = ({
   cat,
   data,
+  isLoading,
   onChange,
   onSubmit,
   testcasesOptions,
 }) => {
   const { name, testcases } = data;
+
+  const isEditForm = cat === "edit";
 
   // handles input field change
   const onInputChange = (e) => onChange(e.target.name, e.target.value);
@@ -24,38 +29,46 @@ const AddNewTestsuite = ({
   const onSelectChange = (value, { name }) => onChange(name, value);
 
   // check whether all fields are filled and used enable/disable the save button
-  const isSaveButtonDisabled = !name || testcases?.length === 0;
+  const isSaveButtonDisabled = isLoading || !name || testcases?.length === 0;
 
   const nameBottomInfoText =
     name && `Your testsuite will be created as ${convertToSlug(name)}`;
 
+  const viewComponentTitle = isEditForm ? name : "Add New Testsuite";
+
   return (
     <Form className="w-100">
       <ViewComponent
-        title={formTitle(cat)}
+        title={viewComponentTitle}
         type="save"
         onSave={onSubmit}
         isSaveDisabled={isSaveButtonDisabled}
       >
-        <FormInput
-          label="Name"
-          placeholder="Name"
-          name="name"
-          value={name}
-          onChange={onInputChange}
-          text={nameBottomInfoText}
-          disabled={cat === "edit"}
-          isRequired
-        />
-        <FormSelect
-          label="Testcases"
-          name="testcases"
-          options={testcasesOptions}
-          value={testcases}
-          onChange={onSelectChange}
-          isRequired
-          isMulti
-        />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <>
+            <FormInput
+              label="Name"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={onInputChange}
+              text={nameBottomInfoText}
+              maxLength={NAME_FIELD_MAX_LENGTH}
+              isRequired
+            />
+            <FormSelect
+              label="Testcases"
+              name="testcases"
+              options={testcasesOptions}
+              value={testcases}
+              onChange={onSelectChange}
+              isRequired
+              isMulti
+            />
+          </>
+        )}
       </ViewComponent>
     </Form>
   );
@@ -70,6 +83,7 @@ AddNewTestsuite.defaultProps = {
 
 AddNewTestsuite.propTypes = {
   cat: PropTypes.oneOf(["add", "edit"]).isRequired,
+  isLoading: PropTypes.bool,
   data: PropTypes.shape({
     name: PropTypes.string,
     testcases: PropTypes.arrayOf(PropTypes.object),
