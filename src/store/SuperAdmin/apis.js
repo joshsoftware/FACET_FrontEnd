716668189abccef8355bf687962db09514ae3dc2
poojan naik <poojannaik88@gmail.com>
2023-02-24 15:09:57 +0530

    bulk add admins route url updated for superadmin

--- src/store/SuperAdmin/apis.js
+++ src/store/SuperAdmin/apis.js
@@ -2,5 +2,5 @@ import { POST, SERVER_URL } from "constants/appConstants";
 import { apiClient } from "store/apiHelper";
 
 export const addAdminsApi = (data) => {
-  return apiClient(`${SERVER_URL}/api/auth/admins/add`, POST, data);
+  return apiClient(`${SERVER_URL}/api/auth/admins`, POST, data);
 };
